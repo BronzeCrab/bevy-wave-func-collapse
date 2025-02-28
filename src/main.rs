87e272399d693918c8353b5c0de71470c1eb8933
Alexey <gr4k@bk.ru>
2025-02-28 21:16:32 +0300

    Init, basic sprite display

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+use bevy::render::settings::*;
+use bevy::render::RenderPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(RenderPlugin {
+                render_creation: WgpuSettings {
+                    backends: Some(Backends::VULKAN),
+                    ..default()
+                }
+                .into(),
+                ..default()
+            }),
+        )
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite::from_image(asset_server.load("down.png")));
+}
