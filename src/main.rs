fa80cff4b28992bf1f2892bdbeeace89bf7cf1d8
Alexey <gr4k@bk.ru>
2025-03-03 21:32:19 +0300

    A lot of bug fixing and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use rand::Rng;
+// use std::{thread, time};
+use rand::prelude::*;
 
 use bevy::prelude::*;
 use bevy::render::settings::*;
@@ -21,8 +22,8 @@ fn main() {
         .run();
 }
 
-#[derive(Debug, Clone)]
-enum Option {
+#[derive(Debug, Clone, PartialEq)]
+enum TileOption {
     Blank,
     Down,
     Left,
@@ -33,7 +34,9 @@ const NUM_OF_OPTIONS: u8 = 4;
 #[derive(Debug, Clone)]
 struct Tile {
     collapsed: bool,
-    options: Vec<Option>,
+    options: Vec<TileOption>,
+    i: usize,
+    j: usize,
 }
 const DIM: usize = 2;
 const SPRITE_SIZE: f32 = 50.;
@@ -43,6 +46,16 @@ fn check_possible_i_and_j(i: i32, j: i32) -> bool {
         return true;
     }
     false
+}
+
+fn find_intesection(a: Vec<TileOption>, b: Vec<TileOption>) -> Vec<TileOption> {
+    let mut res: Vec<TileOption> = vec![];
+    for a_opt in a {
+        if b.contains(&a_opt) {
+            res.push(a_opt);
+        }
+    }
+    res
 }
 
 fn update_near_cells_options(
@@ -59,15 +72,16 @@ fn update_near_cells_options(
                 let poss_left_j: i32 = j as i32 - 1;
                 if check_possible_i_and_j(poss_left_i, poss_left_j) {
                     let possible_left_ind: i32 = poss_left_i * (DIM as i32) + poss_left_j;
-                    let new_left_cell_opt: Vec<Option> = match collapsed_cell.options[0] {
-                        Option::Blank => vec![Option::Blank, Option::Left],
-                        Option::Down => vec![Option::Down, Option::Right],
-                        Option::Left => vec![Option::Down, Option::Right],
-                        Option::Right => vec![Option::Blank, Option::Left],
+                    let new_left_cell_opt: Vec<TileOption> = match collapsed_cell.options[0] {
+                        TileOption::Blank => vec![TileOption::Blank, TileOption::Left],
+                        TileOption::Down => vec![TileOption::Down, TileOption::Right],
+                        TileOption::Left => vec![TileOption::Down, TileOption::Right],
+                        TileOption::Right => vec![TileOption::Blank, TileOption::Left],
                     };
                     let left_cell: &mut Tile = &mut grid[possible_left_ind as usize];
                     if !left_cell.collapsed {
-                        left_cell.options = new_left_cell_opt;
+                        left_cell.options =
+                            find_intesection(left_cell.options.clone(), new_left_cell_opt);
                     }
                 }
 
@@ -75,15 +89,16 @@ fn update_near_cells_options(
                 let poss_right_j: i32 = j as i32 + 1;
                 if check_possible_i_and_j(poss_right_i, poss_right_j) {
                     let possible_right_ind: i32 = poss_right_i * (DIM as i32) + poss_right_j;
-                    let new_right_cell_opt: Vec<Option> = match collapsed_cell.options[0] {
-                        Option::Blank => vec![Option::Blank, Option::Right],
-                        Option::Down => vec![Option::Down, Option::Left],
-                        Option::Left => vec![Option::Blank, Option::Right],
-                        Option::Right => vec![Option::Down, Option::Left],
+                    let new_right_cell_opt: Vec<TileOption> = match collapsed_cell.options[0] {
+                        TileOption::Blank => vec![TileOption::Blank, TileOption::Right],
+                        TileOption::Down => vec![TileOption::Down, TileOption::Left],
+                        TileOption::Left => vec![TileOption::Blank, TileOption::Right],
+                        TileOption::Right => vec![TileOption::Down, TileOption::Left],
                     };
                     let right_cell: &mut Tile = &mut grid[possible_right_ind as usize];
                     if !right_cell.collapsed {
-                        right_cell.options = new_right_cell_opt;
+                        right_cell.options =
+                            find_intesection(right_cell.options.clone(), new_right_cell_opt);
                     }
                 }
 
@@ -91,15 +106,20 @@ fn update_near_cells_options(
                 let poss_top_j: i32 = j as i32;
                 if check_possible_i_and_j(poss_top_i, poss_top_j) {
                     let possible_top_ind: i32 = poss_top_i * (DIM as i32) + poss_top_j;
-                    let new_top_cell_opt: Vec<Option> = match collapsed_cell.options[0] {
-                        Option::Blank => vec![Option::Blank],
-                        Option::Down => vec![Option::Blank],
-                        Option::Left => vec![Option::Down, Option::Left, Option::Right],
-                        Option::Right => vec![Option::Down, Option::Left, Option::Right],
+                    let new_top_cell_opt: Vec<TileOption> = match collapsed_cell.options[0] {
+                        TileOption::Blank => vec![TileOption::Blank],
+                        TileOption::Down => vec![TileOption::Blank],
+                        TileOption::Left => {
+                            vec![TileOption::Down, TileOption::Left, TileOption::Right]
+                        }
+                        TileOption::Right => {
+                            vec![TileOption::Down, TileOption::Left, TileOption::Right]
+                        }
                     };
                     let top_cell: &mut Tile = &mut grid[possible_top_ind as usize];
                     if !top_cell.collapsed {
-                        top_cell.options = new_top_cell_opt;
+                        top_cell.options =
+                            find_intesection(top_cell.options.clone(), new_top_cell_opt);
                     }
                 }
 
@@ -107,15 +127,16 @@ fn update_near_cells_options(
                 let poss_btm_j: i32 = j as i32;
                 if check_possible_i_and_j(poss_btm_i, poss_btm_j) {
                     let possible_bottom_ind: i32 = poss_btm_i * (DIM as i32) + poss_btm_j;
-                    let new_bottom_cell_opt: Vec<Option> = match collapsed_cell.options[0] {
-                        Option::Blank => vec![Option::Blank, Option::Down],
-                        Option::Down => vec![Option::Left, Option::Right],
-                        Option::Left => vec![Option::Left, Option::Right],
-                        Option::Right => vec![Option::Left, Option::Right],
+                    let new_bottom_cell_opt: Vec<TileOption> = match collapsed_cell.options[0] {
+                        TileOption::Blank => vec![TileOption::Blank, TileOption::Down],
+                        TileOption::Down => vec![TileOption::Left, TileOption::Right],
+                        TileOption::Left => vec![TileOption::Left, TileOption::Right],
+                        TileOption::Right => vec![TileOption::Left, TileOption::Right],
                     };
                     let bottom_cell: &mut Tile = &mut grid[possible_bottom_ind as usize];
                     if !bottom_cell.collapsed {
-                        bottom_cell.options = new_bottom_cell_opt;
+                        bottom_cell.options =
+                            find_intesection(bottom_cell.options.clone(), new_bottom_cell_opt);
                     }
                 }
             }
@@ -133,35 +154,57 @@ fn all_cell_collapsed(grid: &Vec<Tile>) -> bool {
 }
 
 fn do_collapse_random_with_low_entropy(grid: &mut Vec<Tile>) -> (usize, usize) {
-    let mut indexes_2_collapse: Vec<usize> = vec![];
-    let mut low_entropy_of_first: usize = 0;
-    let mut i_j_idexes: Vec<(usize, usize)> = vec![];
-    'outer_loop: for i in 0..DIM {
+    let mut lowest_entropy: usize = usize::MAX;
+    for i in 0..DIM {
         for j in 0..DIM {
             let grid_ind: usize = i * DIM + j;
             let grid_cell: &Tile = &grid[grid_ind];
             if !grid_cell.collapsed {
-                if low_entropy_of_first == 0 {
-                    low_entropy_of_first = grid_cell.options.len();
-                } else if grid_cell.options.len() > low_entropy_of_first {
-                    break 'outer_loop;
+                if grid_cell.options.len() < lowest_entropy {
+                    lowest_entropy = grid_cell.options.len();
                 }
-                indexes_2_collapse.push(grid_ind);
-                i_j_idexes.push((i, j));
             }
         }
     }
 
+    let mut indexes_2_collapse: Vec<usize> = vec![];
+    for i in 0..DIM {
+        for j in 0..DIM {
+            let grid_ind: usize = i * DIM + j;
+            let grid_cell: &Tile = &grid[grid_ind];
+            if !grid_cell.collapsed && grid_cell.options.len() == lowest_entropy {
+                indexes_2_collapse.push(grid_ind);
+            }
+        }
+    }
+
+    if indexes_2_collapse.len() <= 0 {
+        panic!("ERROR: indexes_2_collapse is empty");
+    }
     let r_index = rand::rng().random_range(0..=indexes_2_collapse.len() - 1);
     let ind_2_collapase: usize = indexes_2_collapse[r_index];
     let tile: &mut Tile = &mut grid[ind_2_collapase];
     tile.collapsed = true;
 
-    let r_opt_index = rand::rng().random_range(0..=tile.options.len() - 1);
-    let opt: &Option = &tile.options[r_opt_index];
-    tile.options = vec![opt.clone()];
+    if tile.options.len() <= 0 {
+        panic!("ERROR: tile {:?} has zero TileOptions", tile);
+    }
+    // TODO: here we need not just take random, but also check if this
+    // TileOptions is ineed possible!
+    let mut rng = rand::rng();
+    tile.options.shuffle(&mut rng);
+    let mut opt_to_collapse: Option<TileOption> = None;
+    for opt in &tile.options {
+        // TODO: check opt for possible collapse
+        opt_to_collapse = Some(opt.clone());
+        break;
+    }
+    if opt_to_collapse == None {
+        panic!("ERROR: cant find TileOption to collapse tile {:?}", tile);
+    }
+    tile.options = vec![opt_to_collapse.unwrap()];
 
-    i_j_idexes[r_index]
+    (tile.i, tile.j)
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -198,21 +241,30 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         for j in 0..DIM {
             if i == random_cell_i && j == random_cell_j {
                 let r_option_index: u8 = rand::rng().random_range(0..=NUM_OF_OPTIONS - 1);
-                let opt: Option = match r_option_index {
-                    0 => Option::Blank,
-                    1 => Option::Down,
-                    2 => Option::Left,
-                    3 => Option::Right,
+                let opt: TileOption = match r_option_index {
+                    0 => TileOption::Blank,
+                    1 => TileOption::Down,
+                    2 => TileOption::Left,
+                    3 => TileOption::Right,
                     _ => panic!(),
                 };
                 grid.push(Tile {
                     collapsed: true,
                     options: vec![opt],
+                    i: i,
+                    j: j,
                 });
             } else {
                 grid.push(Tile {
                     collapsed: false,
-                    options: vec![Option::Blank, Option::Down, Option::Left, Option::Right],
+                    options: vec![
+                        TileOption::Blank,
+                        TileOption::Down,
+                        TileOption::Left,
+                        TileOption::Right,
+                    ],
+                    i: i,
+                    j: j,
                 });
             }
         }
@@ -222,14 +274,18 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     // second stage, here we need to update entropy of near cells:
     update_near_cells_options(&mut grid, random_cell_i, random_cell_j);
-    println!("grid after update_near_cells_options {:?}", grid);
+    println!("grid after update_near_cells_TileOptions {:?}", grid);
 
+    println!("entering the loop...");
     // third stage, main loop
     while !all_cell_collapsed(&grid) {
-        grid.sort_by(|a, b| a.options.len().partial_cmp(&b.options.len()).unwrap());
         let i_j_tuple: (usize, usize) = do_collapse_random_with_low_entropy(&mut grid);
+        println!("grid after do_collapse_random_with_low_entropy {:?}", grid);
         update_near_cells_options(&mut grid, i_j_tuple.0, i_j_tuple.1);
+        println!("grid after update_near_cells_TileOptions {:?}", grid);
     }
+
+    println!("grid after all cells collapsed {:?}", grid);
 
     // last stage, just display results
     let mut y_shift = 0.;
@@ -239,18 +295,17 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             let grid_ind: usize = i * DIM + j;
             let grid_cell: &Tile = &grid[grid_ind];
             if grid_cell.collapsed && grid_cell.options.len() == 1 {
-                let r_index = rand::rng().random_range(0..=grid_cell.options.len() - 1);
-                let option: &Option = &grid_cell.options[r_index];
-                let sprite: &Sprite = match option {
-                    Option::Blank => &sprites[0],
-                    Option::Down => &sprites[1],
-                    Option::Left => &sprites[2],
-                    Option::Right => &sprites[3],
+                let tile_option: &TileOption = &grid_cell.options[0];
+                let sprite: &Sprite = match tile_option {
+                    TileOption::Blank => &sprites[0],
+                    TileOption::Down => &sprites[1],
+                    TileOption::Left => &sprites[2],
+                    TileOption::Right => &sprites[3],
                 };
-                commands.spawn((sprite.clone(), Transform::from_xyz(x_shift, y_shift, 0.)));
+                commands.spawn((sprite.clone(), Transform::from_xyz(x_shift, -y_shift, 0.)));
             } else {
                 panic!(
-                    "ERROR: {}, {}, i={i}, j={j}, options={:?}",
+                    "ERROR: {}, {}, i={i}, j={j}, TileOptions={:?}",
                     grid_cell.collapsed,
                     grid_cell.options.len(),
                     grid_cell.options
